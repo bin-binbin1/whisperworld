b670f13b05fc29f272ccdbc474cdcb1cb6218dd8
Fang Wenjie <136095426+bin-binbin1@users.noreply.github.com>
2023-10-31 23:21:42 +0800

    first commit

---
+++ src/main/java/com/example/whisperworld/WhisperworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.whisperworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhisperworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhisperworldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/whisperworld/WhisperworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whisperworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhisperworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
