54ead626a465660caed488f9b319a3ecac9758ad
Fang Wenjie <136095426+bin-binbin1@users.noreply.github.com>
2023-11-01 23:00:48 +0800

    add controller and basic htmls

---
+++ src/main/java/com/example/whisperworld/controller/MyController.java
@@ -0,0 +1,28 @@
+package com.example.whisperworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyController {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("name", "World");
+        return "public/index";
+    }
+
+    @GetMapping("/loginpage")
+    public String test(Model model){
+        return "static/test";
+    }
+
+    @PostMapping("/login")
+    public String doLogin(@RequestParam("username") String username, @RequestParam("password") String password, Model model) {
+        // 在这里添加你的登录逻辑
+        // ...
+        return "home";
+    }
+}
---
+++ src/main/java/com/example/whisperworld/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.whisperworld.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/templates/public/**","/").permitAll() // 允许公开访问的URL
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/loginpage") // 自定义登录页面的路径
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    // 其他配置...
+}
