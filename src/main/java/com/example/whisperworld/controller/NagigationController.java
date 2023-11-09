b598e977c552ad69c755f02a5ab64059033bf01c
Linlincode <hulinzhen02281@outlook.com>
2023-11-09 19:04:08 +0800

    a

--- src/main/java/com/example/whisperworld/controller/NagigationController.java
+++ src/main/java/com/example/whisperworld/controller/NagigationController.java
@@ -3,7 +3,10 @@ package com.example.whisperworld.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class NagigationController {
