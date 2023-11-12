df1b096cccb94e42835b781b78e4fd9d43c5bb91
Fang Wenjie <136095426+bin-binbin1@users.noreply.github.com>
2023-11-12 22:12:58 +0800

    fix navigation

--- src/main/java/com/example/whisperworld/config/SecurityConfig.java
+++ src/main/java/com/example/whisperworld/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/public/**", "/login/submit","/register_submit2", "/static/**","/websocket-endpoint/**").permitAll() // 允许公开访问的URL
+                .antMatchers("/public/**","/","/welcome","/login", "/login/submit","/register_submit2", "/static/**","/websocket-endpoint/**").permitAll() // 允许公开访问的URL
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
