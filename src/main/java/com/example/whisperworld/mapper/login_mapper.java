2f2499243ba4b3c496097ac8e2f77271c6a9da03
Ronnie <2092507275@qq.com>
2023-11-15 23:37:42 +0800

    modify htmls

--- src/main/java/com/example/whisperworld/mapper/login_mapper.java
+++ src/main/java/com/example/whisperworld/mapper/login_mapper.java
@@ -11,7 +11,7 @@ public interface login_mapper {
     @Select("SELECT userPassword,userID from users where userID=#{userID}")
     User login_pwd(Integer userID);//匹配用户密码
 
-    @Select("SELECT userID from users where userName=#{username} ")git 
+    @Select("SELECT userID from users where userName=#{username} ")
     Integer login_id(String username);//查找用户id
 
     @Select("SELECT supervisorId from supervisors where supervisorId=#{userID}")
