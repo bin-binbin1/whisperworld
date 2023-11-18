ddeaf78d50569de3f4881ac4e8a39b7f2c3b2c8d
Fang Wenjie <136095426+bin-binbin1@users.noreply.github.com>
2023-11-18 23:38:49 +0800

    stream control

--- src/main/java/com/example/whisperworld/controller/topicsController.java
+++ src/main/java/com/example/whisperworld/controller/topicsController.java
@@ -8,16 +8,12 @@ import com.example.whisperworld.specialClasses.topics;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.listener.Topic;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class topicsController {
@@ -26,25 +22,20 @@ public class topicsController {
 
     @GetMapping("/api/getTopics")//获取全部话题
     public ResponseEntity<String> showTopics(){
-        System.out.println("获取全部话题");
-        List<topics> responses = topicService.showTopics();
-        ObjectMapper mapper = new ObjectMapper();
-        String json="";
-        try {
-            json = mapper.writeValueAsString(responses); // 将Map对象转换为JSON字符串
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return new ResponseEntity<>(json, HttpStatus.OK);
+        List<topics> responses = topicService.showTopics(0,5);
+        return new ResponseEntity<>(topicsToJson(responses), HttpStatus.OK);
+    }
+    @GetMapping("/api/getMoreTopics/{length}")
+    public ResponseEntity<String> showMoreTopics(@PathVariable int length){
+        List<topics> responses = topicService.showTopics(length,10);
+        return new ResponseEntity<>(topicsToJson(responses), HttpStatus.OK);
     }
     @GetMapping("/api/getComments/{topicId}")//获取全部评论
     public ResponseEntity<String> showComments(@PathVariable Integer topicId){
-        System.out.println("获取全部评论");
         Topics topic = new Topics();
         topic.setTopicId(topicId);
         List<TopicReplies> topicReplies = topicService.showTopicReplies(topic);
         List<Map<String, Object>> responses = new ArrayList<>();
-
         // 遍历通知对象数组，并将每个对象的值添加到 Map 中
         for (TopicReplies topicReply : topicReplies) {
             Map<String, Object> response = new HashMap<>();
@@ -68,7 +59,6 @@ public class topicsController {
 
     @PostMapping("/api/sendtopic")//发布话题
     public ResponseEntity<String> sendTopic(@RequestBody Topics topic, @SessionAttribute("loginID") Integer loginID){
-        System.out.println("发布话题");
         topic.setUserId(loginID);
         boolean response = topicService.postTopic(topic);
         ObjectMapper mapper = new ObjectMapper();
@@ -83,10 +73,6 @@ public class topicsController {
 
     @PostMapping("/api/submitComment")//发布评论
     public ResponseEntity<String> sendCommend(@RequestBody TopicReplies topicreplie, @SessionAttribute("loginID") Integer loginID){
-        //前端传递 topicId
-        System.out.println("发布评论");
-//        TopicReplies topicreplie = new TopicReplies();
-
         topicreplie.setCommentUserId(loginID);
         boolean response = topicService.postComment(topicreplie);
         ObjectMapper mapper = new ObjectMapper();
@@ -101,7 +87,6 @@ public class topicsController {
 
     @PostMapping("/api/likeTopic")//点赞
     public ResponseEntity<String> likeTopic(@RequestBody Topics topic, @SessionAttribute("loginID") Integer loginID){
-        System.out.println("点赞");
         int response = topicService.likeTopic(topic,loginID);
         ObjectMapper mapper = new ObjectMapper();
         String json="";
@@ -111,5 +96,16 @@ public class topicsController {
             e.printStackTrace();
         }
         return new ResponseEntity<>(json, HttpStatus.OK);
+    }
+    private String topicsToJson(List<topics> topics){//inverse list
+        Collections.reverse(topics);
+        ObjectMapper mapper = new ObjectMapper();
+        String json="";
+        try {
+            json = mapper.writeValueAsString(topics); // 将Map对象转换为JSON字符串
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return json;
     }
 }
