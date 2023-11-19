819d10035afa1cdd31e6daf35b8a55fc6faa0d1e
Fang Wenjie <136095426+bin-binbin1@users.noreply.github.com>
2023-11-19 11:56:00 +0800

    add crowdsmember entity

---
+++ src/main/java/com/example/whisperworld/entity/CrowdsMembers.java
@@ -0,0 +1,10 @@
+package com.example.whisperworld.entity;
+
+import lombok.Data;
+
+@Data
+public class CrowdsMembers {
+    private Integer GroupID;
+    private Integer MemberID;
+    private boolean STATE;
+}
